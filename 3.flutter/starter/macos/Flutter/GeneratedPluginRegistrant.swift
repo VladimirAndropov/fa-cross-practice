1ab1e21106cbf687f8b0607415a1796428850ecc
BogutaO <olegboguta0409@yandex.ru>
2024-09-10 13:02:31 +0300

    s232323232:

--- 3.flutter/starter/macos/Flutter/GeneratedPluginRegistrant.swift
+++ 3.flutter/starter/macos/Flutter/GeneratedPluginRegistrant.swift
@@ -6,7 +6,11 @@ import FlutterMacOS
 import Foundation
 
 import file_selector_macos
+import path_provider_foundation
+import tflite_flutter_helper
 
 func RegisterGeneratedPlugins(registry: FlutterPluginRegistry) {
   FileSelectorPlugin.register(with: registry.registrar(forPlugin: "FileSelectorPlugin"))
+  PathProviderPlugin.register(with: registry.registrar(forPlugin: "PathProviderPlugin"))
+  TfliteFlutterHelperPlugin.register(with: registry.registrar(forPlugin: "TfliteFlutterHelperPlugin"))
 }
