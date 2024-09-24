dc4fed319b0a07d40fbde2fb85094c1bcef2c9c4
Dmitriy Kondrashov <dm_kondrasov@vcot.info>
2024-09-24 12:55:11 +0300

    flutter red hello world

---
+++ flutter_red_hello_world/README.md
@@ -0,0 +1,3 @@
+# flutter_application_2
+
+A new Flutter project.
---
+++ flutter_red_hello_world/lib/main.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+// void main() {
+//   runApp(const MainApp());
+// }
+
+// class MainApp extends StatelessWidget {
+//   const MainApp({super.key});
+
+//   @override
+//   Widget build(BuildContext context) {
+//     return const MaterialApp(
+//       home: Scaffold(
+//         body: Center(
+//           child: Text('Hello World!'),
+//         ),
+//       ),
+//     );
+//   }
+// }
+
+void main() {
+  runApp(const MaterialApp(
+    home: Center(
+      child: Text(
+        "Привет!",
+        style: TextStyle(
+          color: Colors.red, 
+          decoration: TextDecoration.none,
+        ),
+      ),
+    ),
+  ));
+}
