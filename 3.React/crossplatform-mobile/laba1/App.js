7f08c4d525ec38808ce99f8461a15303d5a56e6c
eln02 <drannikovaelena6@gmail.com>
2024-10-15 12:32:50 +0300

    add 3 lab

--- 3.React/crossplatform-mobile/laba1/App.js
+++ 3.React/crossplatform-mobile/laba1/App.js
@@ -1,68 +1,50 @@
-import { StatusBar } from 'expo-status-bar';
+//import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, ScrollView, SafeAreaView } from 'react-native';
+import { StyleSheet, Text, View, Image } from 'react-native';
+import Onboarding from 'react-native-onboarding-swiper';
+
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <SafeAreaView style={{ flex: 1 }}>
-        {/* Добавляем отступ сверху */}
-        <View style={styles.firstContainer}>
-          <Text style={[styles.textStyle, styles.boldText, styles.bigText]}>5 книжных новинок октября</Text>
-          <StatusBar style="auto" />
-        </View>
-        <View style={styles.secondContainer}>
-          <Text style={[styles.textStyle, styles.bigText]}>
-            «Кадиш.сот» Натан Ингландер. Издательство «Книжники»
-          </Text>
-        </View>
-        <View style={styles.thirdContainer}>
-          <Text style={styles.textStyle}>
-            Ироничная новелла Натана Ингландера, две личные истории культовой Патти Смит, репортаж британской журналистки о будущем человечества, дебютный роман Оушена Вуонга и журналистское расследование о создании «Моссада». В нашей подборке рассказываем о пяти захватывающих книжных новинках, которые достойны того, чтобы появиться на ваших полках.
-          </Text>
-        </View>
-      </SafeAreaView>
-    </View>
+    <Onboarding
+      pages={[
+        {
+          backgroundColor: '#fff',
+          titleStyles: {
+            color: '#3E6266',
+          },
+          image: <Image style={{ width: 300, height: 300 }} source={require('./assets/img/cat3.jpg')} />,
+          title: 'Первая картинка',
+          subtitle: '',
+        },
+        {
+          backgroundColor: '#fff',
+          titleStyles: {
+            color: '#3E6266',
+          },
+          image: <Image style={{ width: 300, height: 300 }} source={require('./assets/img/cat4.jpg')} />,
+          title: 'Вторая картинка',
+          subtitle: '',
+        },
+        {
+          backgroundColor: '#fff',
+          titleStyles: {
+            color: '#3E6266',
+          },
+          image: <Image style={{ width: 300, height: 300 }} source={require('./assets/img/cat5.jpg')} />,
+          title: 'Третья картинка',
+          subtitle: '',
+        },
+      ]}
+    />
   );
 }
 
 const styles = StyleSheet.create({
-  safeArea: {
-    flex: 1,
-  },
-  textStyle: {
-    marginTop: 20,
-    marginLeft: 20,
-    marginRight: 20,
-    textAlign: 'center',
-  },
-  boldText: {
-    fontWeight: 'bold',
-  },
-  bigText: {
-    fontSize: 20,
-  },
   container: {
-    flex: 1,
-    // backgroundColor: '#A9A9FF',
-  },
-  firstContainer: {
     flex: 1,
     backgroundColor: '#fff',
     alignItems: 'center',
     justifyContent: 'center',
-    paddingTop: 40, // Добавляем отступ сверху
-  },
-  secondContainer: {
-    flex: 2,
-    backgroundColor: '#D3D3D3',
-    alignItems: 'center',
-    justifyContent: 'center',
-  },
-  thirdContainer: {
-    flex: 10,
-    backgroundColor: '#A9A9A9',
-    alignItems: 'center',
-    // justifyContent: 'center',
   },
 });
