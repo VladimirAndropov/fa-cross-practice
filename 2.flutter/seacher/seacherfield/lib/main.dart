96dbdc19061f40c489cb50cdfee5fd5be99a50ed
vladimirandropov <vvvvvvvvvv@inbox.ru>
2024-12-06 16:19:55 +0300

    seacher

--- 2.flutter/seacher/seacherfield/lib/main.dart
+++ 2.flutter/seacher/seacherfield/lib/main.dart
@@ -1,95 +1,138 @@
 import 'package:flutter/material.dart';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 
-/// Flutter code sample for [SearchAnchor].
+import 'debounced_search_bar.dart';
 
-const Duration fakeAPIDuration = Duration(seconds: 1);
 
-void main() => runApp(const SearchAnchorAsyncExampleApp());
+void main() {
+  runApp(const MyApp());
+}
 
-class SearchAnchorAsyncExampleApp extends StatelessWidget {
-  const SearchAnchorAsyncExampleApp({super.key});
+class ITunesItem {
+  String wrapperType;
+  String kind;
+  String trackName;
+  String artistName;
+  String collectionName;
+  String? artworkUrl30;
+  String? artworkUrl60;
+  String? artworkUrl100;
 
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      home: Scaffold(
-        appBar: AppBar(
-          title: const Text('SearchAnchor - async'),
-        ),
-        body: const Center(
-          child: _AsyncSearchAnchor(),
-        ),
-      ),
+  ITunesItem({
+    required this.wrapperType,
+    required this.kind,
+    required this.trackName,
+    required this.artistName,
+    required this.collectionName,
+    this.artworkUrl30,
+    this.artworkUrl60,
+    this.artworkUrl100,
+  });
+
+  factory ITunesItem.fromJson(Map<String, dynamic> json) {
+    return ITunesItem(
+      wrapperType: json['wrapperType'] as String,
+      kind: json['kind'] as String,
+      trackName: json['trackName'] as String,
+      artistName: json['artistName'] as String,
+      collectionName: json['collectionName'] as String,
+      artworkUrl30: json['artworkUrl30'] as String?,
+      artworkUrl60: json['artworkUrl60'] as String?,
+      artworkUrl100: json['artworkUrl100'] as String?,
     );
   }
 }
 
-class _AsyncSearchAnchor extends StatefulWidget {
-  const _AsyncSearchAnchor();
+Future<Iterable<ITunesItem>> searchITunes(String query) async {
+  if (query.isEmpty) {
+    return <ITunesItem>[];
+  }
 
-  @override
-  State<_AsyncSearchAnchor> createState() => _AsyncSearchAnchorState();
-}
+  final response = await http.get(
+    Uri.parse('https://itunes.apple.com/search?term=$query&media=music&limit=10'),
+  );
 
-class _AsyncSearchAnchorState extends State<_AsyncSearchAnchor> {
-  // The query currently being searched for. If null, there is no pending
-  // request.
-  String? _searchingWithQuery;
-
-  // The most recent options received from the API.
-  late Iterable<Widget> _lastOptions = <Widget>[];
-
-  @override
-  Widget build(BuildContext context) {
-    return SearchAnchor(
-        builder: (BuildContext context, SearchController controller) {
-      return IconButton(
-        icon: const Icon(Icons.search),
-        onPressed: () {
-          controller.openView();
-        },
-      );
-    }, suggestionsBuilder:
-            (BuildContext context, SearchController controller) async {
-      _searchingWithQuery = controller.text;
-      final List<String> options =
-          (await _FakeAPI.search(_searchingWithQuery!)).toList();
-
-      // If another search happened after this one, throw away these options.
-      // Use the previous options instead and wait for the newer request to
-      // finish.
-      if (_searchingWithQuery != controller.text) {
-        return _lastOptions;
-      }
-
-      _lastOptions = List<ListTile>.generate(options.length, (int index) {
-        final String item = options[index];
-        return ListTile(
-          title: Text(item),
-        );
-      });
-
-      return _lastOptions;
-    });
+  try {
+    if (response.statusCode == 200) {
+      final data = jsonDecode(response.body);
+      final results = List<Map<String, dynamic>>.from(data['results']);
+      return results.map((result) => ITunesItem.fromJson(result)).toList();
+    } else {
+      throw Exception('Error searching iTunes: ${response.statusCode} ${response.reasonPhrase}');
+    }
+  } catch (error) {
+    print(error);
+    return <ITunesItem>[];
   }
 }
 
-// Mimics a remote API.
-class _FakeAPI {
-  static const List<String> _kOptions = <String>[
-    'aardvark',
-    'bobcat',
-    'chameleon',
-  ];
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
-  // Searches the options, but injects a fake "network" delay.
-  static Future<Iterable<String>> search(String query) async {
-    await Future<void>.delayed(fakeAPIDuration); // Fake 1 second delay.
-    if (query == '') {
-      return const Iterable<String>.empty();
-    }
-    return _kOptions.where((String option) {
-      return option.contains(query.toLowerCase());
-    });
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Debounced Search Bar',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+      ),
+      home: const MyHomePage(title: 'Flutter Debounced Search Bar'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key, required this.title});
+  final String title;
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  ITunesItem? _selectedITunesItem;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
+        title: Text(widget.title),
+      ),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            DebouncedSearchBar<ITunesItem>(
+              hintText: 'Search iTunes for music',
+              onResultSelected: (ITunesItem result) {
+                setState(() {
+                  _selectedITunesItem = result;
+                });
+              },
+              resultToString: (ITunesItem result) => result.trackName,
+              resultTitleBuilder: (ITunesItem result) => Text(result.trackName),
+              resultSubtitleBuilder: (ITunesItem result) => Text(result.artistName),
+              resultLeadingBuilder: (ITunesItem result) => result.artworkUrl30 != null
+                  ? Image.network(result.artworkUrl30!)
+                  : const Icon(Icons.music_note),
+              searchFunction: searchITunes,
+            ),
+            if (_selectedITunesItem != null) ...[
+              const SizedBox(height: 16),
+              Text(_selectedITunesItem!.trackName, style: Theme.of(context).textTheme.titleLarge),
+              Text(_selectedITunesItem!.artistName, style: Theme.of(context).textTheme.titleSmall),
+              if (_selectedITunesItem!.artworkUrl100 != null) ...[
+                const SizedBox(height: 16),
+                Image.network(_selectedITunesItem!.artworkUrl100!),
+              ],
+            ],
+          ],
+        ),
+      ),
+    );
   }
 }
